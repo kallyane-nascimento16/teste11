b8639de203052b141df9bf0c325bc0e5cb78f899
kallyane-nascimento16 <110573013+kallyane-nascimento16@users.noreply.github.com>
2022-09-28 18:33:53 -0300

    Add files via upload

---
+++ variaveis/aula01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class aula1
+{
+    public static void maid(string[]args)
+    {
+        Console.WriteLine("ola seja bem-vindo");
+
+        int idade = 10;
+        int Idade;
+        int idadee = (10 * 10) - (10 / 2);
+        int nota1 = 1;
+        int nota2 = 1;
+        int resultado = nota1 + nota2;
+
+        Console.WriteLine("AQUI ESTA O SEU RESULTADO:"+resultado);
+        Console.ReadLine();
+
+
+
+
+    }
+}
